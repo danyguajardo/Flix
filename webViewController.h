046ed6666cc52baa8c490ef9106d224712485506
Daniela Guajardo <danyguajiba@gmail.com>
2019-06-28 12:32:49 -0700

    Add files via upload

---
+++ webViewController.h
@@ -0,0 +1,18 @@
+//
+//  webViewController.h
+//  Flix
+//
+//  Created by danyguajiba on 6/28/19.
+//  Copyright © 2019 danyguajiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface webViewController : UIViewController
+@property (nonatomic, strong) NSString *movieUnique;
+
+@end
+
+NS_ASSUME_NONNULL_END
